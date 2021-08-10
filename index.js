0be9938b3f96ca7a967b5d3f137aa805c7fa4206
almcarvalho <antoniolucas@live.com>
2021-08-09 23:52:37 -0300

    conectando a api da nasa

---
+++ index.js
@@ -0,0 +1,41 @@
+const { ApolloServer, gql } = require('apollo-server');
+const PlanetsAPI = require("./planetsAPI");
+
+
+const resolvers = {
+  Query: {
+    planets: async (_, __, { dataSources }) => {
+      return dataSources.planetsAPI.suitablePlanets();
+    },
+  },
+};
+
+
+const typeDefs = gql`
+  type Planet {
+    pl_name: String
+    pl_bmasse: String
+  }
+
+  #example
+  type Query {
+    planets: [Planet]
+  }
+`;
+
+
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  dataSources: () => {
+    return {
+      planetsAPI: new PlanetsAPI()
+    };
+  }
+});
+
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
